eed0ec5e9b6d9b1b133c1f8861967840920a6cff
Kyle Halleman <kylehalleman@users.noreply.github.com>
2021-12-07 12:26:24 -0600

    Updates

---
+++ precache-manifest.b70f92837bcd3f41bbd92876c3c00364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab2607c2ea2ff888fa954959eaa18af",
+    "url": "/cover-dashboard/index.html"
+  },
+  {
+    "revision": "bcd4433eef12c392f3bb",
+    "url": "/cover-dashboard/static/css/main.922e31fe.chunk.css"
+  },
+  {
+    "revision": "dd06d632103ff63c45f0",
+    "url": "/cover-dashboard/static/js/2.c317edb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cover-dashboard/static/js/2.c317edb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcd4433eef12c392f3bb",
+    "url": "/cover-dashboard/static/js/main.b549bf14.chunk.js"
+  },
+  {
+    "revision": "7f243a7faff7a0e5c62e",
+    "url": "/cover-dashboard/static/js/runtime-main.a2572e98.js"
+  }
+]);
