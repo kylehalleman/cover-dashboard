ff6c66509c5f9f83e89d64025a67ceae1b68dbf3
Kyle Halleman <kylehalleman@users.noreply.github.com>
2021-12-07 12:18:19 -0600

    feat: pare down dashboard to essential features

--- src/App.tsx
+++ src/App.tsx
@@ -1,105 +1,167 @@
-import React, { FormEvent, useMemo, useReducer } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import "./App.css";
 
-import useTeams from "./hooks/useTeams";
+type DataRes = Array<{
+	Lineups: Array<{
+		Team: string;
+		NickName: string;
+		Points: number;
+	}>;
+}>;
 
-import Form from "./components/Form";
-import Teams from "./components/Teams";
-import Players from "./components/Players";
-import APIContext from "./utils/APIContext";
+type TeamWithPoints = {
+	city: string;
+	name: string;
+	points: number[];
+};
 
-interface FormElements extends HTMLFormControlsCollection {
-	"access-token": HTMLInputElement;
-	"league-id": HTMLInputElement;
-	"start-week": HTMLInputElement;
-	"end-week": HTMLInputElement;
+function getTeamData(data: DataRes) {
+	const teamsWithPoints = data.reduce((acc, matchup) => {
+		const [homeTeam, awayTeam] = matchup.Lineups;
+		const homeTeamName = `${homeTeam.Team} ${homeTeam.NickName}`;
+		const awayTeamName = `${awayTeam.Team} ${awayTeam.NickName}`;
+
+		if (!acc[homeTeamName]) {
+			acc[homeTeamName] = {
+				city: homeTeam.Team,
+				name: homeTeam.NickName,
+				points: [homeTeam.Points],
+			};
+		} else {
+			acc[homeTeamName].points.push(homeTeam.Points);
+		}
+
+		if (!acc[awayTeamName]) {
+			acc[awayTeamName] = {
+				city: awayTeam.Team,
+				name: awayTeam.NickName,
+				points: [awayTeam.Points],
+			};
+		} else {
+			acc[awayTeamName].points.push(awayTeam.Points);
+		}
+
+		return acc;
+	}, {} as { [teamName: string]: TeamWithPoints });
+
+	const teamsWithDerivedData: [string, TeamData][] = Object.entries(
+		teamsWithPoints
+	).map(([team, { points, ...rest }]) => {
+		const total = points.reduce((acc, next) => acc + next, 0);
+		return [
+			team,
+			{
+				...rest,
+				best: points.reduce((max, next) => (next > max ? next : max)),
+				worst: points.reduce((min, next) => (next < min ? next : min)),
+				total,
+				average: (total / points.length).toFixed(1),
+			},
+		];
+	});
+
+	teamsWithDerivedData.sort(([, a], [, b]) => b.total - a.total);
+
+	return teamsWithDerivedData;
 }
 
-const initialState = {
-	leagueId: "",
-	authToken: "",
-	startWeek: 0,
-	endWeek: 0,
-};
+type Status = "idle" | "loading" | "success" | "error";
 
-type State = typeof initialState;
-type Action = {
-	type: "update";
-	payload: State;
-};
+function useFetch(url: string, options?: RequestInit) {
+	const [status, setStatus] = useState<Status>("idle");
+	const [data, setData] = useState<Array<[string, TeamData]>>();
 
-function init(initialState: State) {
+	useEffect(() => {
+		setStatus("loading");
+		fetch(url, options)
+			.then((res) => {
+				if (res.ok) {
+					return res.json();
+				} else {
+					throw new Error("Response not OK");
+				}
+			})
+			.then((data) => {
+				setStatus("success");
+				setData(getTeamData(data));
+			})
+			.catch((err) => {
+				setStatus("error");
+			});
+	}, [options, url]);
+
 	return {
-		leagueId: localStorage.getItem("leagueId") || "",
-		authToken: localStorage.getItem("authToken") || "",
-		startWeek: parseInt(localStorage.getItem("startWeek") || "0"),
-		endWeek: parseInt(localStorage.getItem("endWeek") || "0"),
+		status,
+		data,
 	};
 }
 
-function reducer(state: State, { type, payload }: Action) {
-	switch (type) {
-		case "update":
-			const nextState = {
-				...state,
-				...payload,
-			};
-			Object.entries(nextState).forEach(([key, value]) => {
-				localStorage.setItem(key, String(value));
-			});
-			return nextState;
-		default:
-			return state;
+type TeamData = {
+	city: string;
+	name: string;
+	total: number;
+	average: string;
+	best: number;
+	worst: number;
+};
+
+function Teams() {
+	// otherwise you'll just keep on fetching
+	const renderDate = useRef(new Date().toISOString());
+	const { data: teams = [], status } = useFetch(
+		`https://api.cover5.com/v1/fixtures?pagesize=1000&endDate=${renderDate.current}&startDate=2021-09-09T06:00:00Z`
+	);
+
+	switch (status) {
+		case "idle":
+			return null;
+		case "loading":
+			return <p>Loading teams data...</p>;
+		case "error":
+			return <p>Error loading teams!</p>;
+		case "success":
+			return (
+				<>
+					<table>
+						<caption>All Teams</caption>
+						<thead>
+							<tr>
+								<th>Team</th>
+								<th>Best</th>
+								<th>Worst</th>
+								<th>Average</th>
+								<th>Total</th>
+							</tr>
+						</thead>
+						<tbody>
+							{teams.map(
+								([key, { city, name, total, best, worst, average }]) => {
+									return (
+										<tr key={key}>
+											<td>
+												{city} <b>{name}</b>
+											</td>
+											<td>{best}</td>
+											<td>{worst}</td>
+											<td>{average}</td>
+											<td>{total}</td>
+										</tr>
+									);
+								}
+							)}
+						</tbody>
+					</table>
+				</>
+			);
 	}
 }
 
 export default function App() {
-	const [{ leagueId, authToken, startWeek, endWeek }, dispatch] = useReducer(
-		reducer,
-		initialState,
-		init
-	);
-
-	const teams = useTeams(leagueId, authToken, startWeek, endWeek);
-	const context = useMemo(() => ({ authToken, leagueId, startWeek, endWeek }), [
-		authToken,
-		endWeek,
-		leagueId,
-		startWeek,
-	]);
-
-	function handleFormSubmit(e: FormEvent<HTMLFormElement>) {
-		e.preventDefault();
-		const elements = e.currentTarget.elements as FormElements;
-
-		dispatch({
-			type: "update",
-			payload: {
-				leagueId: elements["league-id"].value,
-				authToken: elements["access-token"].value,
-				startWeek: parseInt(elements["start-week"].value),
-				endWeek: parseInt(elements["end-week"].value),
-			},
-		});
-	}
-
 	return (
 		<main className="container">
-			<header>
-				<h1>Cover5 Dashboard</h1>
-			</header>
-			<Form
-				onSubmit={handleFormSubmit}
-				defaultLeagueId={leagueId}
-				defaultAuthToken={authToken}
-				defaultStartWeek={String(startWeek)}
-				defaultEndWeek={String(endWeek)}
-			/>
-			<APIContext.Provider value={context}>
-				<Teams teams={teams} />
-				<Players teams={teams} />
-			</APIContext.Provider>
+			<h1>Cover5 Dashboard</h1>
+			<Teams />
 		</main>
 	);
 }
--- src/utils/APIContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-export default createContext({
-	authToken: "",
-	leagueId: "",
-	startWeek: 0,
-	endWeek: 0,
-});
